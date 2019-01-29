44e841de2b729feb955947a6d0c854d5c91a9c36
AlexandrTolstov <alextav_85@mail.ru>
2019-01-29 20:42:10 +0600

    1

--- MutexSynchro/Program.cs
+++ MutexSynchro/Program.cs
@@ -2,14 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MutexSynchro
 {
     class Program
     {
+        static Mutex mutexObj = new Mutex();
+        static int x = 0;
         static void Main(string[] args)
         {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread myThread = new Thread(Count);
+                myThread.Name = "Поток " + i.ToString();
+                myThread.Start();
+            }
+            Console.ReadLine();
+        }
+
+        private static void Count()
+        {
+            mutexObj.WaitOne();
+            x = 1;
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine("{0}: {1}", Thread.CurrentThread.Name, x);
+                x++;
+                Thread.Sleep(100);
+            }
+            mutexObj.ReleaseMutex();
         }
     }
 }
